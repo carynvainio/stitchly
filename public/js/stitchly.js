da661f10bbdede508432ed713ac05cd6d3e8ed6c
Caryn <caryn.vainio@gmail.com>
2016-05-05 20:50:01 -0700

    Added stitch symbol images for stitch chart

--- public/js/stitchly.js
+++ public/js/stitchly.js
@@ -4,19 +4,16 @@
 // and do some stuff with the values
 var chart_rows = 10;
 var chart_cols = 5; 
-
-var selected_stitch = "X";
+var selected_stitch_id = 0;
 var selected_cell_id;
+var keyboard_numbers = [49,50,51,52,53,54,55,56,57,48];  // 0-9
 
-var keyboard_numbers = [48,49,50,51,52,53,54,55,56,57];  // 0-9
+// in a hardcorded array for now while I test
+var stitches = ["", "img-stitch_yo.png", "img-stitch_purl.png", "img-stitch_k2tog.png", "img-stitch_ssk.png", "img-stitch_s1-k2tog-psso.png"];
 
 createChart(5, 10);
 setSelectedChartCell(chart_cols-1,chart_rows-1);
-
-// set up the onclick for the stitch toolbar
-$(".stitch-selection").click(function() {
-        selectStitchFromToolbar( $( this ).attr('id') );
-    });
+createStitchToolbar();
 
 // listen for keyboard input
 // right = 39     left = 37     up = 38     down = 40       enter = 13
@@ -32,7 +29,7 @@ $(document).keydown(function(event){
         } else if (event.which == 13) {
             markSelectedStitch(selected_cell_id);
         } else if ($.inArray(event.which, keyboard_numbers) != -1) {
-            selectStitchFromToolbar("stitch" + $.inArray(event.which, keyboard_numbers));
+            selectStitchFromToolbar("stitch-" + $.inArray(event.which, keyboard_numbers));
         }
     });
 
@@ -56,18 +53,40 @@ function createChart(cols, rows) {
     }
 }
 
+function createStitchToolbar() {
+    var parent = $('#symbols');
+    var keyparent = $('#keys');
+
+    for (var i = 0; i < stitches.length; i++) {
+        var stitch = $('<div />', {
+            }).addClass('stitch-selection').appendTo(parent);
+        stitch.attr('id', 'stitch-' + i);
+        stitch.css("background-image", "url(../public/img/stitches/" + stitches[i] + ")");  
+        stitch.click(function() {
+                selectStitchFromToolbar( $( this ).attr('id') );
+            });
+
+        var stitchkey = $('<div />', {
+            }).addClass('key').appendTo(keyparent);
+        stitchkey.text(i+1);
+    }
+}
+
 // Select a stitch from the toolbar
 function selectStitchFromToolbar(stitch_id) {
-    var selected_stitch_cell = $( '#' + stitch_id );
+    var stitchnum = parseInt(stitch_id.substring(stitch_id.indexOf("-")+1));
+    if ( stitchnum < stitches.length) {
+        var selected_stitch_cell = $( '#' + stitch_id );
 
-    // set the new selected stitch
-    selected_stitch = selected_stitch_cell.text();
+        // set the new selected stitch
+        selected_stitch_id = stitchnum;
 
-    // reset all the other stitches to not-selected
-    $('.stitchbar-row').children().attr('class', 'stitch-selection');
+        // reset all the other stitches to not-selected
+        $('#symbols').children().attr('class', 'stitch-selection');
 
-    // highlight the selected stitch in the toolbar
-    selected_stitch_cell.attr( 'class', 'stitch-selection stitch-selected');
+        // highlight the selected stitch in the toolbar
+        selected_stitch_cell.attr( 'class', 'stitch-selection stitch-selected');
+    }
 }
 
 // Mark the selected stitch in the cell we clicked on
@@ -75,7 +94,7 @@ function markSelectedStitch(cell_id) {
     var sel_stitch_cell = $( '#' + cell_id );
 
     // mark the stitch in the cell
-    sel_stitch_cell.text(selected_stitch);
+    sel_stitch_cell.css("background-image", "url(../public/img/stitches/" + stitches[selected_stitch_id] + ")"); 
     var arr = getChartCellPosById(cell_id);
     setSelectedChartCell(arr[0], arr[1]);
 }
@@ -121,8 +140,6 @@ function getChartCellPosById(c_id) {
     var col = parseInt(c_id.substring(c_id.indexOf("c")+1));
     var row = parseInt(c_id.substring(c_id.indexOf("r")+1,c_id.indexOf("-")));
     arr = [col, row];
-
-    //console.log("col = " + arr[0] + "  row: " + arr[1]);
 
     return arr;
 }
