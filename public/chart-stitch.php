935ba6a880c3429f5711a3ceb0b014a6800e7e43
Caryn <caryn.vainio@gmail.com>
2016-05-15 13:47:48 -0700

    Added container-stitches

--- public/chart-stitch.php
+++ public/chart-stitch.php
@@ -9,14 +9,15 @@ include('header.php');
         <p class="lead">A chart to play with to see if the interactions feel good.</p>
 
         <!-- STITCH TOOLBAR -->
-        <div class="containerstitches">
-          <div class="stitchbar">
-            <div class="stitchbar-row" id="symbols"></div>
-            <div class="stitchbar-row" id="keys"></div>
-          </div>
-          <div class="edit_options" style="visibility: hidden">Edit symbols</div>
+        <div class="container-stitches">
+            <!-- <div class="edit-options"><a>Edit Colors</a></div> -->
+            <div class="stitchbar">
+              <div class="stitchbar-row" id="symbols"></div>
+              <div class="stitchbar-row" id="keys"></div>
+            </div>
         </div>
 
+        <!-- CHART -->
         <div class="container-chart">
           <div class="chart">
           </div>
