3f0602b664e88ea2a7722db0ff554076df8a5e50
Caryn <caryn.vainio@gmail.com>
2016-05-20 20:51:03 -0700

    Version notes update 0.0.2

--- public/version-notes.php
+++ public/version-notes.php
@@ -1,5 +1,15 @@
 <div class="container_version-notes">
-	<h4>Current Version Notes (0.1.0)</h4>
+	<h4>0.0.2 Version Notes</h4>
+	<ul>
+	    <li>Refactored the code to avoid doing some dumb things</li>
+	    <li>Fixed some bugs related to using the keyboard to enter stitches</li>
+	</ul>
+	<b>Known Issues:</b>
+	<ul>
+		<li>Some issues relating to the colorPicker flickering on/off as you toggle between Edit Colors and Done</li>
+	</ul>
+	<br />
+	<h4>0.0.1 Version Notes</h4>
 	<ul>
 	    <li>Added custom color setting to colors in the Color Stitch Toolbar</li>
 	    <li>Rows no longer wrap on charts when the browser is narrower than the cart</li>
