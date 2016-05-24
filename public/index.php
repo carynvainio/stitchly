0500286cd092987200293e6ee3d91aaa6e7d6803
Caryn Vainio <caryn@statelessnetworks.com>
2016-05-23 21:48:27 -0700

    Created sign-up page

--- public/index.php
+++ public/index.php
@@ -1,19 +1,75 @@
-<?php
-include('header.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/favicon.ico">
+
+    <title>Stitchly: Create Charts for Knitting and Needlework</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- mailchimp signup form -->
+    <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+
+
+        <!-- Stitchly stylesheets -->
+    <link href="css/stitchly-sass.css" rel="stylesheet">
+    <link id="colorPickerMod" rel="stylesheet" type="text/css" href="css/CPmod.css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+
+    <!-- font awesome -->
+    <script src="https://use.fontawesome.com/1cbacccd3d.js"></script>
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body class="cover">
 
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hello! Welcome to what will eventually be Stitchly.</h1>
-        <p class="lead">How are you today? Good.</p>
-        <p>This is a very, <em>very</em> bare-bones prototype of Stitchly, an app I'm working on to create charts for knitting other charted needlework. If you're here, then it means I've probably sent you the link to ask if you can play around with it and give me feedback. So thanks for spending your time to help me develop it! I'm interested in any feedback you have, no matter how small. If you find something frustrating, then I want to know about it. If you find yourself thinking, "gee, I really wish that it let me do..." then I want to know that, too. So please <a href="mailto:caryn.vainio@gmail.com">give me a shout!</a></p>
+        <h1 class="brand">Stitchly</h1>
+        <p class="lead brand-page">Create charts for knitting, needlework and more.</p>
+        <p class="brand-page">If you're a knitter, needleworker, or crocheter looking for a well-designed web app to help you make charts for your projects, we hope Stitchly is what you're looking for! Stitchly is coming soon, but we're asking some of our knitting friends to try out our early prototypes and give us feedback to help make Stitchly the right tool for you. Sign up below to find out when!</p>
 
-        <p>- Caryn Vainio</p>
+        <p class="brand-page">- Caryn Vainio</p>
 
-            <?php
-            include('version-notes.php');
-            ?>
+            <!-- Begin MailChimp Signup Form -->
+            
+            <div id="mc_embed_signup">
+            <form action="//elementalbellydance.us7.list-manage.com/subscribe/post?u=4ddda6bd5e90a116d1647df34&amp;id=f21d488127" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                <label for="mce-EMAIL">Subscribe to our mailing list</label>
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4ddda6bd5e90a116d1647df34_f21d488127" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                </div>
+            </form>
+            </div>
+
+            <!--End mc_embed_signup-->
 
       </div>
 
