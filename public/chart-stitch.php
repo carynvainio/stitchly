de22d30d921be9eadeea51547889e92e92d4e38b
Caryn Vainio <caryn@statelessnetworks.com>
2016-05-27 16:33:07 -0700

    Modified stitchbar/tool layout

--- public/chart-stitch.php
+++ public/chart-stitch.php
@@ -10,6 +10,7 @@ include('header.php');
 
     <div class="container">
 
+      <!--
       <div class="starter-template">
         <p class="lead">A chart to play with to see if the interactions feel good.</p>
         <p>Feedback? Send it to <a href="mailto:caryn.vainio@gmail.com">Caryn Vainio!</a></p>
@@ -19,13 +20,26 @@ include('header.php');
             <li><b>Mark stitches:</b> Click to mark a single stitch, or click and drag to mark multiple stitches. Clicking on a stitch you just marked will reset the stitch. You can also mark a stitch by hitting the Enter key.</li>
           </ul> 
       </div>
+    -->
 
         <!-- STITCH TOOLBAR -->
-        <div class="container-stitches">
-            <!-- <div class="edit-options"><a>Edit Colors</a></div> -->
-            <div class="stitchbar">
-            </div>
-        </div>
+        <table class="container-stitches">
+          <tr>
+            <th class="col1" colspan="2">Stitches</th>
+            <th class="col3"></th>
+          </tr>
+          <tr>
+            <td class="stitch-editing-options">
+              <a class="edit-colors"><i class="fa fa-pencil fa-lg" aria-hidden="true"></i></a>
+              <!-- <a class="edit-done"><i class="fa fa-check fa-lg" aria-hidden="true"></i></a>
+              <a class="edit-cancel"><i class="fa fa-times fa-lg" aria-hidden="true"></i></a> -->
+            </td>
+            <td class="stitchbar"><div id="stitchbar"><!-- STITCHBAR --></div></td>
+            <td class="macro-editing-options">
+              <a class="edit-clear"><i class="fa fa-undo fa-lg" aria-hidden="true"></i></a>
+            </td>
+          </tr>
+        </table>
 
         <!-- CHART -->
         <div class="container-chart">
